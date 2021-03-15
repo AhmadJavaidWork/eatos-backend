b35da897e7a65f510d1790913b93121f971f9cc4
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2021-03-15 17:06:00 +0300

    changed apis configuration due to mongo to pg

--- src/services/knex/knexConfig.js
+++ src/services/knex/knexConfig.js
@@ -1,13 +1,13 @@
-require('dotenv').config({
-  path: `${process.env.PWD}/${process.env.NODE_ENV || 'development'}.env`,
-});
-
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
   };
 } else {
+  require('dotenv').config({
+    path: `${process.env.PWD}/${process.env.NODE_ENV || 'development'}.env`,
+  });
+
   module.exports = {
     development: {
       client: 'pg',
