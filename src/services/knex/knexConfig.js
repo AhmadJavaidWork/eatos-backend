231cc61bd17cd5d8ec71d74af1fd83716f680ffe
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2021-03-15 17:03:29 +0300

    changed apis configuration due to mongo to pg

--- src/services/knex/knexConfig.js
+++ src/services/knex/knexConfig.js
@@ -2,21 +2,28 @@ require('dotenv').config({
   path: `${process.env.PWD}/${process.env.NODE_ENV || 'development'}.env`,
 });
 
-module.exports = {
-  development: {
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
     client: 'pg',
-    connection: {
-      host: process.env.POSTGRES_HOST,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
+    connection: process.env.PG_CONNECTION_STRING,
+  };
+} else {
+  module.exports = {
+    development: {
+      client: 'pg',
+      connection: {
+        host: process.env.POSTGRES_HOST,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+      },
+      migrations: {
+        directory: `${process.env.PWD}/db/migrations`,
+        tableName: 'knex_migartion',
+      },
+      seeds: {
+        directory: `${process.env.PWD}/db/seeds`,
+      },
     },
-    migrations: {
-      directory: `${process.env.PWD}/db/migrations`,
-      tableName: 'knex_migartion',
-    },
-    seeds: {
-      directory: `${process.env.PWD}/db/seeds`,
-    },
-  },
-};
+  };
+}
